dfb07474d0254461af0bf09b97c9996f40798d91
Ana <anamestree@gmail.com>
2020-04-24 12:54:47 +0200

    First lab: first exercise solved
    Write a simple client-server program that can be run as two different processes (nodes) that by just changing the port number in the listing interfaces and the port and possibly IP addresses in the receiving sockets, they can send string messages to each other using TCP connections

---
+++ lab1/ex1/Readme.txt
@@ -0,0 +1,17 @@
+# Advanced Topics in Distributed Systems
+
+## Lab 1: Introduction to Distributed Systems Programming
+### Files:
+- machine0.go: Code for one machine (server & client).
+- machine1.go: Code for one machine (server & client). Ports are exchanged regarding machine0's ports.
+- machine0: Executable for machine0.go
+- machine1: Executable for machine1.go
+
+### How to compile:
+This action is not necessary since the executables are already provided.
+- go build machine0.go
+- go build machine1.go
+
+### How to execute:
+- Open a terminal, execute machine0: ./machine0
+- Open another terminal, execute machine1: ./machine1
---
+++ lab1/ex1/machine0.go
@@ -0,0 +1,72 @@
+package main
+
+import ("net"
+        "fmt"
+        "bufio"
+        "os"
+        "time"
+        "strings")
+
+
+/*
+  Creates a server using the Port "port".
+*/
+func server(port string){
+  fmt.Println("Launching server...")
+  // listen on all interfaces,
+  ln, _ := net.Listen("tcp", port)
+  // accept connection on port
+  conn, _ := ln.Accept()
+  // run loop forever (or until ctrl-c)
+  for {
+    // will listen for message to process ending in newline (\n)
+    message, err := bufio.NewReader(conn).ReadString('\n')
+    if err != nil { break }
+    // output message received
+    fmt.Print("--- Message Received:", string(message))
+    // sample process for string received
+    newmessage := strings.ToUpper(message)
+    // send new string back to client
+    conn.Write([]byte(newmessage + "\n"))
+  }
+}
+
+
+/*
+  Connects with a server with the IP "ip" (which already contains the port).
+*/
+func client(ip string){
+  // connect to this socket -> Dial = creates the connection
+  fmt.Print("Checking if server has started" + "\n")
+  conn, err := net.Dial("tcp", ip)
+  // Handle error: try to connect again until server is up and running
+  if err != nil {
+    fmt.Print("Error: ", err)
+    fmt.Print("\n")
+    time.Sleep(time.Second)
+    client(ip)
+  }  else {
+    fmt.Print("Server Up and Running" + "\n")
+    for {
+      // read in input from stdin
+      reader := bufio.NewReader(os.Stdin)
+      fmt.Print("Text to send: ")
+      text, _ := reader.ReadString('\n')
+      // send to socket
+      fmt.Fprintf(conn, text + "\n")
+      // listen for reply
+      message, err := bufio.NewReader(conn).ReadString('\n')
+      if err != nil { break }
+      fmt.Print("Message from server: " + message)
+    }
+  }
+}
+
+
+
+func main() {
+    port := ":6002" // Port used for creating our own server
+    ip := "127.0.0.1:6001" // Ip used for connecting to the other server
+    go server(port)
+    client(ip)
+}
