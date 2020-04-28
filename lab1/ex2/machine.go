4cb5c80c4d475f34fe583c57809ffd734f87ee3a
Ana <anamestree@gmail.com>
2020-04-28 23:46:17 +0200

    Finally the "stop" is working as expected using channels

--- lab1/ex2/machine.go
+++ lab1/ex2/machine.go
@@ -11,7 +11,7 @@ import ("net"
 /*
   Creates a server using the Port "port".
 */
-func server(port string){
+func server(port string, ch chan string){
   fmt.Println("Launching server in port: " + port + "...")
   // listen on all interfaces,
   ln, _ := net.Listen("tcp", ":"+port)
@@ -20,14 +20,14 @@ func server(port string){
   for {
     // accept connection on port
     conn, _ := ln.Accept()
-    go serverConnection(conn)
+    go serverConnection(conn, ch)
   }
 }
 
 /*
   Handles a connection (conn) for the server.
 */
-func serverConnection(conn net.Conn){
+func serverConnection(conn net.Conn, ch chan string){
   // will listen for message to process ending in newline (\n)
   message, err := bufio.NewReader(conn).ReadString('\n')
   if err != nil {
@@ -36,10 +36,13 @@ func serverConnection(conn net.Conn){
         return
     }
   // output message received
-  temp := strings.TrimSpace(string(message))
-  if strings.ToLower(temp) == "stop"{
-    fmt.Println("client left..")
+  //temp := strings.TrimSpace(string(message))
+  temp := strings.Split(string(message), ";")
+  if strings.ToLower(temp[0]) == "stop"{
+    ip := strings.TrimSpace(string(temp[1]))
+    fmt.Println("client left.." + ip)
     conn.Close()
+    ch <- ip
     return
   } else {
     fmt.Print("--- Message Received:", string(message))
@@ -47,7 +50,7 @@ func serverConnection(conn net.Conn){
     newmessage := strings.ToUpper(message)
     // send new string back to client
     conn.Write([]byte(newmessage + "\n"))
-    serverConnection(conn)
+    serverConnection(conn, ch)
   }
 }
 
@@ -61,8 +64,7 @@ func connect(ip string) net.Conn{
   conn, err := net.Dial("tcp", ip)
   // Handle error: try to connect again until server is up and running
   if err != nil {
-      fmt.Print("Error: ", err)
-      fmt.Print("\n")
+      fmt.Println("Error: ", err)
       time.Sleep(time.Second)
       return connect(ip)
   }  else {
@@ -71,11 +73,16 @@ func connect(ip string) net.Conn{
   }
 }
 
+func waitForStop(ch chan string, connections map[string]net.Conn){
+    elem, _ := <- ch
+    delete(connections, elem)
+}
+
 
 /*
   Connects with a server with the IP "ip" (which already contains the port).
 */
-func client(ips []string){
+func client(ips []string, ch chan string, myIP string){
   connections := map[string]net.Conn{}
   for _, ip := range ips {
     connections[ip] = connect(ip)
@@ -86,10 +93,21 @@ func client(ips []string){
       reader := bufio.NewReader(os.Stdin)
       fmt.Print("Text to send: ")
       text, _ := reader.ReadString('\n')
+      go waitForStop(ch, connections)
+      //fmt.Print("Client loop, connections length: ")
+      //fmt.Println(len(connections))
+      if len(connections) == 0 {
+        //fmt.Println("Len connections == 0")
+        os.Exit(1)
+      }
       temp := strings.TrimSpace(string(text))
       temp = strings.ToLower(temp)
 
       for _, conn := range connections {
+        if temp == "stop"{
+          fmt.Println("Client ip " + myIP + " senting stop")
+          text = "stop;" + myIP
+        }
           fmt.Fprintf(conn, text + "\n")
           // listen for reply
           message, err := bufio.NewReader(conn).ReadString('\n')
@@ -97,6 +115,7 @@ func client(ips []string){
           fmt.Print("Message from server: " + message)
       }
       if temp == "stop"{
+        fmt.Println("Client: Exit due to stop")
         os.Exit(1)
       }
   }
@@ -124,27 +143,94 @@ func getConfig(myFile string) (string, []string){
 
   scanner := bufio.NewScanner(file)
   first := true
-  serverPort := ""
+  serverIP := ""
   ips := []string{}
   for scanner.Scan() {
       if first {
-        serverPort = scanner.Text()
-        serverPort = strings.Split(serverPort, ":")[1]
+        serverIP = scanner.Text()
         first = false
       } else {
         ips = append(ips, scanner.Text())
       }
   }
-  return serverPort, ips
+  return serverIP, ips
 }
 
+/*
+
+type Connection struct {
+	IP string
+	Port string
+  Ch chan string
+}
+
+
+func clients(connections){
+    for {
+      fmt.Print("Text to send: ")
+      text, _ := reader.ReadString('\n')
+      for every connection {
+          envia al seu channel el text
+      }
+    }
+} */
+
+func clients(ips []string, ch chan string, myIP string){
+  connections := map[string]net.Conn{}
+  for _, ip := range ips {
+    connections[ip] = connect(ip)
+  }
+  for {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Text to send: ")
+    text, _ := reader.ReadString('\n')
+    go waitForStop(ch, connections)
+    //fmt.Print("Client loop, connections length: ")
+    //fmt.Println(len(connections))
+    if len(connections) == 0 {
+      //fmt.Println("Len connections == 0")
+      os.Exit(1)
+    }
+    for ip, conn := range connections {
+      go handleClient(conn, text, ip)
+    }
+    temp := strings.TrimSpace(string(text))
+    temp = strings.ToLower(temp)
+    if temp == "stop"{
+      fmt.Println("Client: Exit due to stop")
+      os.Exit(1)
+    }
+  }
+}
+
+
+func handleClient(conn net.Conn, text string, myIP string){
+    temp := strings.TrimSpace(string(text))
+    temp = strings.ToLower(temp)
+    if temp == "stop"{
+      fmt.Println("Client ip " + myIP + " senting stop")
+      text = "stop;" + myIP
+    }
+    fmt.Fprintf(conn, text + "\n")
+    // listen for reply
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    //if err != nil { break }
+    fmt.Print("Message from server: " + message)
+    /*for {
+      //llegeixo del meu channel
+      //envio missatge al server al qual estic connectat
+    }*/
+  }
 
 func main() {
     if len(os.Args) != 2{
       usage()
     } else {
-      port, ips := getConfig(os.Args[1])
-      go server(port)
-      client(ips)
+      myIP, ips := getConfig(os.Args[1])
+      port := strings.Split(myIP, ":")[1]
+      ch := make(chan string)
+      go server(port, ch)
+      //client(ips, ch, myIP)
+      client(ips, ch, myIP)
   }
 }
